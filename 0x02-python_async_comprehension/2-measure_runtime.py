52b0f3813bd1d6f902fc224d7d16c68a194b85e0
japheth-alt163 <japhethbaraka4@gmail.com>
2024-05-07 14:36:56 +0000

    2-measure_runtime.py

---
+++ 0x02-python_async_comprehension/2-measure_runtime.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+"""
+Measure runtime module
+"""
+
+import asyncio
+from time import perf_counter
+from typing import List
+
+
+async_comprehension = __import__('1-async_comprehension').async_comprehension
+
+
+async def measure_runtime() -> float:
+    """
+    Coroutine that measures the total runtime of executing
+    async_comprehension four times in parallel.
+    """
+    start_time = perf_counter()
+    await asyncio.gather(*(async_comprehension() for _ in range(4)))
+    end_time = perf_counter()
+    return end_time - start_time
+
+
+if __name__ == "__main__":
+    import asyncio
+
+    async def main():
+        return await measure_runtime()
+    print(asyncio.run(main()))
